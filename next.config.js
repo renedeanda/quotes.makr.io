a63340d96d111032e7213a4ed9505ab18c165d99
René DeAnda <rene.ideanda@gmail.com>
2024-09-26 11:39:44 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   webpack: (config) => {
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
